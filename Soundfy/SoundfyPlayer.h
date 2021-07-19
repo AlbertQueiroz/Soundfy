42401351133ed6493588bed7e9a9eeb88ad82881
Vinicius Mesquita <viniciuscoelho.ti@gmail.com>
2021-07-19 15:50:13 -0300

    Add SoundPlayer class

---
+++ Soundfy/SoundfyPlayer.h
@@ -0,0 +1,12 @@
+//
+//  SoundfyPlayer.h
+//  Soundfy
+//
+//  Created by Vinicius Mesquita on 19/07/21.
+//
+
+#ifndef SoundfyPlayer_h
+#define SoundfyPlayer_h
+
+
+#endif /* SoundfyPlayer_h */
---
+++ Soundfy/SoundfyPlayer.m
@@ -0,0 +1,8 @@
+//
+//  SoundfyPlayer.m
+//  Soundfy
+//
+//  Created by Vinicius Mesquita on 19/07/21.
+//
+
+#import <Foundation/Foundation.h>
