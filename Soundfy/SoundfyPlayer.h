e9ec7a54a81cbd9dd42a04d38fd8e819d803a9b1
Vinicius Mesquita <viniciuscoelho.ti@gmail.com>
2021-07-20 09:55:02 -0300

    Add playSound function

--- Soundfy/SoundfyPlayer.h
+++ Soundfy/SoundfyPlayer.h
@@ -5,10 +5,12 @@
 //  Created by Vinicius Mesquita on 19/07/21.
 //
 #import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface SoundfyPlayer: NSObject {
+@interface SoundfyPlayer: NSObject <AVAudioPlayerDelegate> {
     double volume;
     NSString *soundName;
 }
 @property(nonatomic, readwrite) double _volume;
+- (void)playSound:(NSString *)soundName;
 @end
--- Soundfy/SoundfyPlayer.m
+++ Soundfy/SoundfyPlayer.m
@@ -7,14 +7,35 @@
 
 #import <Foundation/Foundation.h>
 #import <Soundfy/SoundfyPlayer.h>
+#import <AVFoundation/AVFoundation.h>
 
 @implementation SoundfyPlayer
+
+AVAudioPlayer *player;
 
 -(id)init {
     self = [super init];
     self._volume = 1.0;
     NSLog(@"Hello, the volume is %f", self._volume);
     return self;
+}
+
+-(void)playSound:(NSString *)soundName {
+    // Search for path of sound
+    NSString *path = [[NSBundle mainBundle] pathForResource:soundName ofType:@"mp3"];
+    // Create url with the path
+    NSURL *url = [NSURL fileURLWithPath:path];
+    // Create error variable
+    NSError *error;
+    // Instatiate player with url and error.
+    player = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:&error];
+    player.numberOfLoops = 1;
+    player.delegate = self;
+    [player play];
+}
+
+- (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag {
+    NSLog(@"%d", flag);
 }
 
 @end
